594a0cbde5527876523a88c8b9cb5a66f78bc183
Prince Nayakpara <princepatel27507@gmail.com>
2026-03-10 13:45:32 +0530

    Create 66-PlusOne

---
+++ 66-PlusOne/66-PlusOne.js
@@ -0,0 +1,17 @@
+// Last updated: 10/03/2026, 13:45:32
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+    for (let i = digits.length - 1; i >= 0; i--) {
+        if (digits[i] < 9) {
+            digits[i]++;
+            return digits;
+        }
+        digits[i] = 0;
+    }
+    
+    digits.unshift(1);
+    return digits;
+};
