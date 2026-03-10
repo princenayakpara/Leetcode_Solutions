c60d477e572d5e3ecd62555cc7b08bff90c442d3
Prince Nayakpara <princepatel27507@gmail.com>
2026-03-10 13:45:11 +0530

    Create 242-ValidAnagram

---
+++ 242-ValidAnagram/242-ValidAnagram.js
@@ -0,0 +1,22 @@
+// Last updated: 10/03/2026, 13:45:11
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    if (s.length !== t.length) return false;
+
+    let freq = {};
+
+    for (let ch of s) {
+        freq[ch] = (freq[ch] || 0) + 1;
+    }
+
+    for (let ch of t) {
+        if (!freq[ch]) return false;
+        freq[ch]--;
+    }
+
+    return true; 
+};
