db512ac0906890381fbd73cfac4e75fe5bd2b7c4
Prince Nayakpara <princepatel27507@gmail.com>
2026-03-10 13:44:58 +0530

    Create 389-FindTheDifference

---
+++ 389-FindTheDifference/389-FindTheDifference.js
@@ -0,0 +1,20 @@
+// Last updated: 10/03/2026, 13:44:58
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {character}
+ */
+var findTheDifference = function(s, t) {
+    let freq = {};
+
+    for (let ch of s) {
+    freq[ch] = (freq[ch] || 0) + 1;
+}
+
+     for (let ch of t) {
+        if (!freq[ch]) {
+            return ch;
+        }
+        freq[ch]--;
+    }
+};
