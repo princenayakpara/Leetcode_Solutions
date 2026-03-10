f769aa001dd2fc361db820d28ccf894e827c9d22
Prince Nayakpara <princepatel27507@gmail.com>
2026-03-10 13:45:42 +0530

    Create 27-RemoveElement

---
+++ 27-RemoveElement/27-RemoveElement.js
@@ -0,0 +1,18 @@
+// Last updated: 10/03/2026, 13:45:42
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    let k = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== val) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+
+    return k;
+};
