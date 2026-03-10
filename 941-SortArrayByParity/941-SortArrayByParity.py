6bb9eef393a5e99b6f97b5a8a543555a4ccec3fb
Prince Nayakpara <princepatel27507@gmail.com>
2026-03-10 13:44:47 +0530

    Create 941-SortArrayByParity

---
+++ 941-SortArrayByParity/941-SortArrayByParity.py
@@ -0,0 +1,12 @@
+# Last updated: 10/03/2026, 13:44:47
+class Solution(object):
+    def sortArrayByParity(self, nums):
+        j = 0
+
+        for i in range(len(nums)):
+            if nums[i]%2 == 0:
+                nums[i], nums[j] = nums[j], nums[i]
+                j+= 1
+
+        return nums
+        
