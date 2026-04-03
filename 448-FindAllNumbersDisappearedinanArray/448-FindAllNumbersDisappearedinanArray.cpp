9c85fa766faee42b119d4d61d66dededcfebf445
Prince Nayakpara <princepatel27507@gmail.com>
2026-04-03 15:48:31 +0530

    Create 448-FindAllNumbersDisappearedinanArray

---
+++ 448-FindAllNumbersDisappearedinanArray/448-FindAllNumbersDisappearedinanArray.cpp
@@ -0,0 +1,20 @@
+// Last updated: 03/04/2026, 15:48:31
+1class Solution {
+2public:
+3    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+4        unordered_map<string, vector<string>> mp;
+5
+6        for (string s : strs) {
+7            string key = s;
+8            sort(key.begin(), key.end()); 
+9            mp[key].push_back(s);
+10        }
+11
+12        vector<vector<string>> result;
+13        for (auto& it : mp) {
+14            result.push_back(it.second);
+15        }
+16
+17        return result;
+18    }
+19};
