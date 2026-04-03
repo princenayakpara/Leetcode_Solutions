be488f95a44f8d7a9b860240b08d6bffce96d606
Prince Nayakpara <princepatel27507@gmail.com>
2026-04-03 15:27:09 +0530

    File updated at 03/04/2026, 15:27:09

--- 1-TwoSum/1-TwoSum.cpp
+++ 1-TwoSum/1-TwoSum.cpp
@@ -1,17 +1,20 @@
-// Last updated: 02/04/2026, 15:16:34
+// Last updated: 03/04/2026, 15:27:09
 1class Solution {
 2public:
 3    vector<int> twoSum(vector<int>& nums, int target) {
-4        
-5        for(int i = 0; i < nums.size(); i++) {
-6            for(int j = i + 1; j < nums.size(); j++) {
-7                
-8                if(nums[i] + nums[j] == target) {
-9                    return {i, j};
-10                }
-11            }
-12        }
-13
-14        return {}; 
-15    }
-16};
+4        unordered_map<int, int> mp; 
+5        
+6        for (int i = 0; i < nums.size(); i++) {
+7            int diff = target - nums[i];
+8            
+9            
+10            if (mp.find(diff) != mp.end()) {
+11                return {mp[diff], i};
+12            }
+13            
+14            mp[nums[i]] = i;
+15        }
+16        
+17        return {}; 
+18    }
+19};
