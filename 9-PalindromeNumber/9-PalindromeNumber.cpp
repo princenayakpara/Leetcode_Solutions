dbf1c7cfa090affad0b87480b78d72561aadb976
Prince Nayakpara <princepatel27507@gmail.com>
2026-05-03 12:58:43 +0530

    File updated at 03/05/2026, 12:58:42

--- 9-PalindromeNumber/9-PalindromeNumber.cpp
+++ 9-PalindromeNumber/9-PalindromeNumber.cpp
@@ -1,19 +1,25 @@
-// Last updated: 03/05/2026, 12:32:30
+// Last updated: 03/05/2026, 12:58:42
 1class Solution {
 2public:
-3    bool isPalindrome(int x) {
-4        if(x<0){
-5            return false;
-6        }
-7        else{
-8            long long int num = 0;
-9            int ans = x;
-10            while(x>=1){
-11                int temp = x % 10;
-12                num = num * 10 + temp;
-13                x = x/10;
-14            }
-15            return num == ans;
-16        }
-17    }
-18};
+3    vector<string> fizzBuzz(int n) {
+4        vector<string> result;
+5
+6        for(int i = 1; i <= n; i++) {
+7
+8            if(i%3==0 && i%5==0) {
+9                result.push_back("FizzBuzz");
+10            }
+11            else if(i%3==0) {
+12                result.push_back("Fizz");
+13            }
+14            else if(i%5==0) {
+15                result.push_back("Buzz");
+16            }
+17            else {
+18                result.push_back(to_string(i));
+19            }
+20        }
+21
+22        return result;
+23    }
+24};
