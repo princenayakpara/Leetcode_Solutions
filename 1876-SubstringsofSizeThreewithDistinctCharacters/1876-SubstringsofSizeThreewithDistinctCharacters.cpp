13b8745951dbc88d48e31e61982c9cc8e5746dcb
Prince Nayakpara <princepatel27507@gmail.com>
2026-05-07 00:12:21 +0530

    Create 1876-SubstringsofSizeThreewithDistinctCharacters

---
+++ 1876-SubstringsofSizeThreewithDistinctCharacters/1876-SubstringsofSizeThreewithDistinctCharacters.cpp
@@ -0,0 +1,17 @@
+// Last updated: 07/05/2026, 00:12:21
+1class Solution {
+2public:
+3    int countGoodSubstrings(string s) {
+4        int count = 0;
+5
+6        for(int i = 0; i + 2 < s.size(); i++) {
+7            if(s[i] != s[i + 1] &&
+8               s[i] != s[i + 2] &&
+9               s[i + 1] != s[i + 2]) {
+10                count++;
+11            }
+12        }
+13
+14        return count;
+15    }
+16};
