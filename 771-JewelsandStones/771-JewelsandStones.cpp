2359a9b1354bd0bd81e8170a7c90fcb6f5a95137
Prince Nayakpara <princepatel27507@gmail.com>
2026-05-07 21:02:42 +0530

    File updated at 07/05/2026, 21:02:42

--- 771-JewelsandStones/771-JewelsandStones.cpp
+++ 771-JewelsandStones/771-JewelsandStones.cpp
@@ -1,17 +1,14 @@
-// Last updated: 01/05/2026, 16:53:34
+// Last updated: 07/05/2026, 21:02:42
 1class Solution {
 2public:
 3    int numJewelsInStones(string jewels, string stones) {
 4        unordered_set<char> jewelSet(jewels.begin(), jewels.end());
 5        int count = 0;
-6
-7        for (char c : stones) {
-8            if (jewelSet.count(c)) {
-9                count++;
-10            }
-11        }
-12
-13        return count;
-14    }
-15};
-16
+6        for(char c : stones){
+7            if(jewelSet.count(c)){
+8                count++;
+9            }
+10        }
+11        return count;
+12    }
+13};
