fe18ea758cbae2a1ca612d48b5e06e96b1a3eb9a
Prince Nayakpara <princepatel27507@gmail.com>
2026-05-07 22:00:00 +0530

    File updated at 07/05/2026, 22:00:00

--- 917-ReverseOnlyLetters/917-ReverseOnlyLetters.cpp
+++ 917-ReverseOnlyLetters/917-ReverseOnlyLetters.cpp
@@ -1,31 +1,13 @@
-// Last updated: 07/05/2026, 21:57:26
+// Last updated: 07/05/2026, 22:00:00
 1class Solution {
 2public:
-3    string capitalizeTitle(string title) {
-4        int n = title.size();
+3    int titleToNumber(string columnTitle) {
+4        int result = 0;
 5
-6        for (char &c : title) {
-7            c = tolower(c);
+6        for(char c : columnTitle) {
+7            result = result * 26 + (c - 'A' + 1);
 8        }
 9
-10        int i = 0;
-11
-12        while (i < n) {
-13            int start = i;
-14
-15            while (i < n && title[i] != ' ') {
-16                i++;
-17            }
-18
-19            int len = i - start;
-20
-21            if (len > 2) {
-22                title[start] = toupper(title[start]);
-23            }
-24
-25            i++;
-26        }
-27
-28        return title;
-29    }
-30};
+10        return result;
+11    }
+12};
