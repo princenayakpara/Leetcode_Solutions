f8a332159af0408e9d0ac033a485fb5bec03b47e
Prince Nayakpara <princepatel27507@gmail.com>
2026-05-09 02:06:49 +0530

    File updated at 09/05/2026, 02:06:49

--- 83-RemoveDuplicatesfromSortedList/83-RemoveDuplicatesfromSortedList.cpp
+++ 83-RemoveDuplicatesfromSortedList/83-RemoveDuplicatesfromSortedList.cpp
@@ -1,18 +1,7 @@
-// Last updated: 08/05/2026, 22:47:04
+// Last updated: 09/05/2026, 02:06:49
 1class Solution {
 2public:
-3    ListNode* deleteDuplicates(ListNode* head) {
-4        ListNode* temp = head;
-5
-6        while(temp != NULL && temp->next != NULL) {
-7            if(temp->val == temp->next->val) {
-8                temp->next = temp->next->next;
-9            }
-10            else {
-11                temp = temp->next;
-12            }
-13        }
-14
-15        return head;
-16    }
-17};
+3    int countOdds(int low, int high) {
+4        return (high + 1) / 2 - low / 2;
+5    }
+6};
