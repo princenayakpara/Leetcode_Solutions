92add18b5aee7768dd0115134ce4877f51ccba1d
Prince Nayakpara <princepatel27507@gmail.com>
2026-05-08 16:08:13 +0530

    File updated at 08/05/2026, 16:08:13

--- 1945-SumofDigitsofStringAfterConvert/1945-SumofDigitsofStringAfterConvert.cpp
+++ 1945-SumofDigitsofStringAfterConvert/1945-SumofDigitsofStringAfterConvert.cpp
@@ -1,17 +1,14 @@
-// Last updated: 08/05/2026, 16:03:59
+// Last updated: 08/05/2026, 16:08:13
 1class Solution {
 2public:
-3    char findTheDifference(string s, string t) {
-4        int xorValue = 0;
+3    bool detectCapitalUse(string word) {
+4        int upper = 0;
 5
-6        for(char c : s) {
-7            xorValue ^= c;
-8        }
-9
-10        for(char c : t) {
-11            xorValue ^= c;
-12        }
-13
-14        return (char)xorValue;
-15    }
-16};
+6        for(char c : word)
+7            upper += (isupper(c) ? 1 : 0);
+8
+9        return upper == word.size() ||
+10               upper == 0 ||
+11               (upper == 1 && isupper(word[0]));
+12    }
+13};
