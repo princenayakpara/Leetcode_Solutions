ee8bc66972b36d8cccb8d512aa295a0b143f4646
Prince Nayakpara <princepatel27507@gmail.com>
2026-05-08 16:43:22 +0530

    File updated at 08/05/2026, 16:43:21

--- 1957-DeleteCharacterstoMakeFancyString/1957-DeleteCharacterstoMakeFancyString.cpp
+++ 1957-DeleteCharacterstoMakeFancyString/1957-DeleteCharacterstoMakeFancyString.cpp
@@ -1,18 +1,26 @@
-// Last updated: 08/05/2026, 16:37:22
+// Last updated: 08/05/2026, 16:43:21
 1class Solution {
 2public:
-3    string makeFancyString(string s) {
-4        string ans;
+3    int getSum(int n) {
+4        int sum = 0;
 5
-6        for(char c : s) {
-7            int n = ans.size();
-8
-9            if(n >= 2 && ans[n - 1] == c && ans[n - 2] == c)
-10                continue;
+6        while(n) {
+7            int d = n % 10;
+8            sum += d * d;
+9            n /= 10;
+10        }
 11
-12            ans += c;
-13        }
+12        return sum;
+13    }
 14
-15        return ans;
-16    }
-17};
+15    bool isHappy(int n) {
+16        unordered_set<int> seen;
+17
+18        while(n != 1 && !seen.count(n)) {
+19            seen.insert(n);
+20            n = getSum(n);
+21        }
+22
+23        return n == 1;
+24    }
+25};
