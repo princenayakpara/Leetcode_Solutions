db24b80bf172ea9a37d15e6162cadfa5af516e25
Prince Nayakpara <princepatel27507@gmail.com>
2026-05-08 16:04:00 +0530

    File updated at 08/05/2026, 16:03:59

--- 1945-SumofDigitsofStringAfterConvert/1945-SumofDigitsofStringAfterConvert.cpp
+++ 1945-SumofDigitsofStringAfterConvert/1945-SumofDigitsofStringAfterConvert.cpp
@@ -1,38 +1,17 @@
-// Last updated: 08/05/2026, 15:59:20
+// Last updated: 08/05/2026, 16:03:59
 1class Solution {
 2public:
-3    int minMaxDifference(int num) {
-4        string s = to_string(num);
+3    char findTheDifference(string s, string t) {
+4        int xorValue = 0;
 5
-6        // Make maximum number
-7        string maxi = s;
-8        char chMax = 0;
+6        for(char c : s) {
+7            xorValue ^= c;
+8        }
 9
-10        for(char c : s) {
-11            if(c != '9') {
-12                chMax = c;
-13                break;
-14            }
-15        }
-16
-17        if(chMax) {
-18            for(char &c : maxi) {
-19                if(c == chMax) {
-20                    c = '9';
-21                }
-22            }
-23        }
-24
-25        // Make minimum number
-26        string mini = s;
-27        char chMin = s[0];
-28
-29        for(char &c : mini) {
-30            if(c == chMin) {
-31                c = '0';
-32            }
-33        }
-34
-35        return stoi(maxi) - stoi(mini);
-36    }
-37};
+10        for(char c : t) {
+11            xorValue ^= c;
+12        }
+13
+14        return (char)xorValue;
+15    }
+16};
