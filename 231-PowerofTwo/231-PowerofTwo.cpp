028e3ebf875d2c8015e138bb7aa556435946a454
Prince Nayakpara <princepatel27507@gmail.com>
2026-05-09 10:44:14 +0530

    File updated at 09/05/2026, 10:44:14

--- 231-PowerofTwo/231-PowerofTwo.cpp
+++ 231-PowerofTwo/231-PowerofTwo.cpp
@@ -1,13 +1,13 @@
-// Last updated: 09/05/2026, 10:41:51
+// Last updated: 09/05/2026, 10:44:14
 1class Solution {
 2public:
-3    bool isPowerOfTwo(int n) {
-4       while(n>1){
-5        if(n%2 != 0){
-6            return false;
-7        }
-8        n /= 2;
-9       } 
-10       return n == 1;
+3    bool isPowerOfThree(int n) {
+4        while(n > 1){
+5            if(n % 3 != 0){
+6                return false;
+7            }
+8            n /= 3;
+9        }
+10        return n == 1;
 11    }
 12};
