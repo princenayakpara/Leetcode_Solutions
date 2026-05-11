c86ecae184be247009dfa868ee9f8ff26761f9cf
Prince Nayakpara <princepatel27507@gmail.com>
2026-05-11 10:01:41 +0530

    File updated at 11/05/2026, 10:01:42

--- 278-FirstBadVersion/278-FirstBadVersion.cpp
+++ 278-FirstBadVersion/278-FirstBadVersion.cpp
@@ -1,19 +1,8 @@
-// Last updated: 11/05/2026, 09:58:50
+// Last updated: 11/05/2026, 10:01:42
 1class Solution {
 2public:
-3    int firstBadVersion(int n) {
-4        int left = 1, right = n;
-5
-6        while (left < right) {
-7            int mid = left + (right - left) / 2;
-8
-9            if (isBadVersion(mid)) {
-10                right = mid;   // first bad could be mid
-11            } else {
-12                left = mid + 1;
-13            }
-14        }
-15
-16        return left;
-17    }
-18};
+3    bool isPerfectSquare(int num) {
+4        int x = sqrt(num);
+5        return x * x == num;
+6    }
+7};
