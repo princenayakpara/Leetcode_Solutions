e52f129252478e10e66d67c4d35b99f665acbad1
Prince Nayakpara <princepatel27507@gmail.com>
2026-05-18 17:16:48 +0530

    File updated at 18/05/2026, 17:16:47

--- 507-PerfectNumber/507-PerfectNumber.cpp
+++ 507-PerfectNumber/507-PerfectNumber.cpp
@@ -1,21 +1,22 @@
-// Last updated: 18/05/2026, 16:33:56
+// Last updated: 18/05/2026, 17:16:47
 1class Solution {
 2public:
-3    bool checkPerfectNumber(int num) {
-4        if(num == 1) return false;
+3    int firstMissingPositive(vector<int>& nums) {
+4        int n = nums.size();
 5
-6        int sum = 1;
-7
-8        for(int i = 2; i * i <= num; i++) {
-9            if(num % i == 0) {
-10                sum += i;
-11
-12                if(i != num / i) {
-13                    sum += num / i;
-14                }
-15            }
-16        }
-17
-18        return sum == num;
-19    }
-20};
+6        for(int i = 0; i < n; i++) {
+7            while(nums[i] > 0 && nums[i] <= n &&
+8                  nums[i] != nums[nums[i] - 1]) {
+9                swap(nums[i], nums[nums[i] - 1]);
+10            }
+11        }
+12
+13        for(int i = 0; i < n; i++) {
+14            if(nums[i] != i + 1) {
+15                return i + 1;
+16            }
+17        }
+18
+19        return n + 1;
+20    }
+21};
