ee36b0f2625e3397813dfdcdc5b587908cfd40cb
Prince Nayakpara <princepatel27507@gmail.com>
2026-05-23 16:12:57 +0530

    File updated at 23/05/2026, 16:12:57

--- 701-InsertintoaBinarySearchTree/701-InsertintoaBinarySearchTree.cpp
+++ 701-InsertintoaBinarySearchTree/701-InsertintoaBinarySearchTree.cpp
@@ -1,26 +1,37 @@
-// Last updated: 23/05/2026, 16:06:34
-1/**
-2 * Definition for a binary tree node.
-3 * struct TreeNode {
-4 *     int val;
-5 *     TreeNode *left;
-6 *     TreeNode *right;
-7 *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
-8 *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-9 *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-10 * };
-11 */
-12class Solution {
-13public:
-14    TreeNode* insertIntoBST(TreeNode* root, int val) {
-15        if(root == NULL)
-16            return new TreeNode(val);
+// Last updated: 23/05/2026, 16:12:57
+1class Solution {
+2public:
+3    bool isVowel(char ch) {
+4        return ch=='a' || ch=='e' || ch=='i' || 
+5               ch=='o' || ch=='u';
+6    }
+7    
+8    int maxVowels(string s, int k) {
+9         int cnt = 0;
+10        int ans = 0;
+11
+12        // first window
+13        for(int i = 0; i < k; i++) {
+14            if(isVowel(s[i]))
+15                cnt++;
+16        }
 17
-18        if(val < root->val)
-19            root->left = insertIntoBST(root->left, val);
-20        else
-21            root->right = insertIntoBST(root->right, val);
+18        ans = cnt;
+19
+20        // sliding window
+21        for(int i = k; i < s.size(); i++) {
 22
-23        return root;
-24    }
-25};
+23            // remove left character
+24            if(isVowel(s[i-k]))
+25                cnt--;
+26
+27            // add new character
+28            if(isVowel(s[i]))
+29                cnt++;
+30
+31            ans = max(ans, cnt);
+32        }
+33
+34        return ans;
+35    }
+36};
