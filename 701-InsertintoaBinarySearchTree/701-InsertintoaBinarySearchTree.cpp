18566b49fae15852713679891b6af6dada728eea
Prince Nayakpara <princepatel27507@gmail.com>
2026-05-23 16:24:05 +0530

    File updated at 23/05/2026, 16:24:06

--- 701-InsertintoaBinarySearchTree/701-InsertintoaBinarySearchTree.cpp
+++ 701-InsertintoaBinarySearchTree/701-InsertintoaBinarySearchTree.cpp
@@ -1,8 +1,22 @@
-// Last updated: 23/05/2026, 16:14:58
+// Last updated: 23/05/2026, 16:24:06
 1class Solution {
 2public:
-3    int getSum(int a, int b) {
-4        int sum = a + b;
-5        return sum;
-6    }
-7};
+3    int rev(int n) {
+4        int r = 0;
+5
+6        while(n) {
+7            r = r * 10 + n % 10;
+8            n /= 10;
+9        }
+10
+11        return r;
+12    }
+13    bool sumOfNumberAndReverse(int num) {
+14        for(int i = 0; i <= num; i++) {
+15            if(i + rev(i) == num)
+16                return true;
+17        }
+18
+19        return false;
+20    }
+21};
