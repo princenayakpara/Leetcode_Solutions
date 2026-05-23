bfe3b86cfa81acac0583b86171661c3be3ace5aa
Prince Nayakpara <princepatel27507@gmail.com>
2026-05-23 22:40:21 +0530

    File updated at 23/05/2026, 22:40:22

--- 509-FibonacciNumber/509-FibonacciNumber.cpp
+++ 509-FibonacciNumber/509-FibonacciNumber.cpp
@@ -1,10 +1,16 @@
-// Last updated: 23/05/2026, 22:36:07
+// Last updated: 23/05/2026, 22:40:22
 1class Solution {
 2public:
 3    int fib(int n) {
-4        if(n <=1)
-5        return n;
-6
-7        return fib(n - 1) + fib(n - 2);
-8    }
-9};
+4        int a=0,b=1,q,c=0;
+5        if(n==0)
+6        return 0;
+7        if(n==1)
+8        return 1;
+9        for(int i=2;i<=n;i++){
+10        q=a+b;
+11        a=b;
+12        b=q;}
+13        return q;
+14    }
+15};
