e252c5d7617298d706148205bf04b14bc9759a85
Prince Nayakpara <princepatel27507@gmail.com>
2026-05-27 02:34:50 +0530

    File updated at 27/05/2026, 02:34:50

--- 1812-DetermineColorofaChessboardSquare/1812-DetermineColorofaChessboardSquare.cpp
+++ 1812-DetermineColorofaChessboardSquare/1812-DetermineColorofaChessboardSquare.cpp
@@ -1,7 +1,15 @@
-// Last updated: 27/05/2026, 02:25:58
+// Last updated: 27/05/2026, 02:34:50
 1class Solution {
 2public:
-3    bool isSameAfterReversals(int num) {
-4        return num == 0 || num % 10!= 0;
-5    }
-6};
+3    bool halvesAreAlike(string s) {
+4    string vowels = "aeiouAEIOU";
+5    int n = s.size()/2;
+6    int count = 0;
+7
+8    for(int i = 0; i<n; i++){
+9        if(vowels.find(s[i]) != string::npos)count++;
+10        if(vowels.find(s[n+i]) != string::npos)count--;
+11    }
+12    return count == 0;
+13    }
+14};
