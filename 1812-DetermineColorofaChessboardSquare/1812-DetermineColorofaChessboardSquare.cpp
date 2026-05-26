53d4c009300f747aa9bf7abfc4d16009966d5750
Prince Nayakpara <princepatel27507@gmail.com>
2026-05-27 02:39:51 +0530

    File updated at 27/05/2026, 02:39:51

--- 1812-DetermineColorofaChessboardSquare/1812-DetermineColorofaChessboardSquare.cpp
+++ 1812-DetermineColorofaChessboardSquare/1812-DetermineColorofaChessboardSquare.cpp
@@ -1,15 +1,12 @@
-// Last updated: 27/05/2026, 02:34:50
+// Last updated: 27/05/2026, 02:39:51
 1class Solution {
 2public:
-3    bool halvesAreAlike(string s) {
-4    string vowels = "aeiouAEIOU";
-5    int n = s.size()/2;
-6    int count = 0;
-7
-8    for(int i = 0; i<n; i++){
-9        if(vowels.find(s[i]) != string::npos)count++;
-10        if(vowels.find(s[n+i]) != string::npos)count--;
-11    }
-12    return count == 0;
-13    }
-14};
+3    int sumBase(int n, int k) {
+4        int sum = 0;
+5        while(n>0){
+6            sum+=n%k;
+7            n/=k;
+8        }
+9        return sum;
+10    }
+11};
