d8e95662a31593b53c19afdde379175edd265721
Prince Nayakpara <princepatel27507@gmail.com>
2026-05-27 02:19:18 +0530

    File updated at 27/05/2026, 02:19:18

--- 1812-DetermineColorofaChessboardSquare/1812-DetermineColorofaChessboardSquare.cpp
+++ 1812-DetermineColorofaChessboardSquare/1812-DetermineColorofaChessboardSquare.cpp
@@ -1,7 +1,9 @@
-// Last updated: 05/05/2026, 07:34:38
+// Last updated: 27/05/2026, 02:19:18
 1class Solution {
 2public:
 3    bool squareIsWhite(string coordinates) {
-4        return (coordinates[0] + coordinates[1]) % 2 == 1;
-5    }
-6};
+4        int col = coordinates[0]-'a';
+5        int row = coordinates[1]-'0';
+6        return (col + row) % 2== 0;
+7    }
+8};
