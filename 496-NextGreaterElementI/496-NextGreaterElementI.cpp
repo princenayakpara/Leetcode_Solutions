7bfd14155752bafb62c9a864957142fe83aa4789
Prince Nayakpara <princepatel27507@gmail.com>
2026-06-02 11:58:13 +0530

    File updated at 02/06/2026, 11:58:14

--- 496-NextGreaterElementI/496-NextGreaterElementI.cpp
+++ 496-NextGreaterElementI/496-NextGreaterElementI.cpp
@@ -1,32 +1,24 @@
-// Last updated: 02/06/2026, 11:45:03
+// Last updated: 02/06/2026, 11:58:14
 1class Solution {
 2public:
 3    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-4        vector<int> ans;
-5
-6        for (int num : nums1) {
-7            int idx = -1;
-8
-9            // Find num in nums2
-10            for (int i = 0; i < nums2.size(); i++) {
-11                if (nums2[i] == num) {
-12                    idx = i;
-13                    break;
-14                }
-15            }
-16
-17            // Find next greater element
-18            int nextGreater = -1;
-19            for (int i = idx + 1; i < nums2.size(); i++) {
-20                if (nums2[i] > num) {
-21                    nextGreater = nums2[i];
-22                    break;
-23                }
-24            }
-25
-26            ans.push_back(nextGreater);
-27        }
-28
-29        return ans;
-30    }
-31};
+4        unordered_map<int, int> mp;
+5        stack<int> st;
+6
+7        for (int i = nums2.size() - 1; i >= 0; i--) {
+8            while (!st.empty() && st.top() <= nums2[i]) {
+9                st.pop();
+10            }
+11
+12            mp[nums2[i]] = st.empty() ? -1 : st.top();
+13            st.push(nums2[i]);
+14        }
+15
+16        vector<int> ans;
+17        for (int num : nums1) {
+18            ans.push_back(mp[num]);
+19        }
+20
+21        return ans;
+22    }
+23};
