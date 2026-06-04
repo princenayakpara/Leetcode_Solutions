b40c663f61187f23e472c1741e14c5d10cd698c5
Prince Nayakpara <princepatel27507@gmail.com>
2026-06-04 11:15:04 +0530

    File updated at 04/06/2026, 11:15:04

--- 1071-GreatestCommonDivisorofStrings/1071-GreatestCommonDivisorofStrings.cpp
+++ 1071-GreatestCommonDivisorofStrings/1071-GreatestCommonDivisorofStrings.cpp
@@ -1,12 +1,15 @@
-// Last updated: 04/06/2026, 10:55:53
+// Last updated: 04/06/2026, 11:15:04
 1class Solution {
 2public:
-3    string gcdOfStrings(string str1, string str2) {
-4        if(str1 + str2 != str2 + str1)
-5            return "";
-6
-7        int len = gcd(str1.length(), str2.length());
-8
-9        return str1.substr(0, len);
-10    }
-11};
+3    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+4        int mx = *max_element(candies.begin(), candies.end());
+5
+6        vector<bool>ans;
+7
+8        for(int candy : candies){
+9            ans.push_back(candy + extraCandies >= mx);
+10        }
+11
+12        return ans;
+13    }
+14};
