1a6eaf5f7d95ed26d0cbdde6b0a066a6cb1f010a
Prince Nayakpara <princepatel27507@gmail.com>
2026-06-08 10:32:39 +0530

    File updated at 08/06/2026, 10:32:39

--- 228-SummaryRanges/228-SummaryRanges.cpp
+++ 228-SummaryRanges/228-SummaryRanges.cpp
@@ -1,26 +1,16 @@
-// Last updated: 08/06/2026, 09:29:46
+// Last updated: 08/06/2026, 10:32:39
 1class Solution {
 2public:
-3    vector<string> summaryRanges(vector<int>& nums) {
-4         vector<string> ans;
-5        int n = nums.size();
-6
-7        for(int i = 0; i<n; i++){
-8            int start = nums[i];
-9
-10          while (i + 1 < n && nums[i + 1] == nums[i] + 1) {
-11                i++;
-12            }
-13
-14            int end = nums[i];
-15
-16            if(start == end){
-17                ans.push_back(to_string(start));
-18            }else{
-19                ans.push_back(to_string(start) + "->" + to_string(end));
-20            }
-21        }
-22        return ans;
-23
-24    }
-25};
+3    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+4        unordered_map<int, int> mp;
+5
+6        for (int i = 0; i < nums.size(); i++) {
+7            if (mp.count(nums[i]) && i - mp[nums[i]] <= k) {
+8                return true;
+9            }
+10            mp[nums[i]] = i;
+11        }
+12
+13        return false;
+14    }
+15};
