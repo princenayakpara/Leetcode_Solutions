54cae05eb8860333ef67fb9c7c90b78b3b43be82
Prince Nayakpara <princepatel27507@gmail.com>
2026-06-13 13:48:17 +0530

    File updated at 13/06/2026, 13:48:17

--- 1365-HowManyNumbersAreSmallerThantheCurrentNumber/1365-HowManyNumbersAreSmallerThantheCurrentNumber.cpp
+++ 1365-HowManyNumbersAreSmallerThantheCurrentNumber/1365-HowManyNumbersAreSmallerThantheCurrentNumber.cpp
@@ -1,21 +1,15 @@
-// Last updated: 13/06/2026, 13:17:24
+// Last updated: 13/06/2026, 13:48:17
 1class Solution {
 2public:
 3    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
-4        vector<int> ans;
-5
-6        for(int i = 0; i < nums.size(); i++) {
-7            int count = 0;
-8
-9            for(int j = 0; j < nums.size(); j++) {
-10                if(nums[j] < nums[i]) {
-11                    count++;
-12                }
-13            }
-14
-15            ans.push_back(count);
-16        }
-17
-18        return ans;
-19    }
-20};
+4        vector<int> freq(101,0);
+5        for(int num:nums)
+6        freq[num]++;
+7        for(int i=1;i<101;i++)
+8        freq[i]+=freq[i-1];
+9        vector<int> ans;
+10        for(int num:nums)
+11        ans.push_back(num==0? 0:freq[num-1]);
+12        return ans;
+13    }
+14};
