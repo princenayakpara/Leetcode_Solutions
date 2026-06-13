d9344e4882ecc62487d4284a9ce9fda56cc6cc42
Prince Nayakpara <princepatel27507@gmail.com>
2026-06-13 11:31:17 +0530

    File updated at 13/06/2026, 11:31:17

--- 1002-FindCommonCharacters/1002-FindCommonCharacters.cpp
+++ 1002-FindCommonCharacters/1002-FindCommonCharacters.cpp
@@ -1,36 +1,31 @@
-// Last updated: 13/06/2026, 11:24:48
+// Last updated: 13/06/2026, 11:31:17
 1class Solution {
 2public:
 3    vector<string> commonChars(vector<string>& words) {
-4        string res = words[0];
+4        vector<string> res;
 5
-6        for (int j = 0; j < res.size(); ) {
-7            char ch = res[j];
-8            bool common = true;
+6        for (char ch : words[0]) {
+7            string temp = "";
+8            temp += ch;
 9
-10            for (int i = 1; i < words.size(); i++) {
-11                int pos = words[i].find(ch);
-12
-13                if (pos != string::npos) {
-14                    words[i].erase(pos, 1); // remove one occurrence
-15                } else {
-16                    common = false;
+10            bool found = true;
+11
+12            for (int j = 1; j < words.size(); j++) {
+13                size_t pos = words[j].find(ch);
+14
+15                if (pos == string::npos) {
+16                    found = false;
 17                    break;
 18                }
-19            }
-20
-21            if (!common) {
-22                res.erase(j, 1); // remove from result
-23            } else {
-24                j++;
+19
+20                words[j].erase(pos, 1);
+21            }
+22
+23            if (found) {
+24                res.push_back(temp);
 25            }
 26        }
 27
-28        vector<string> ans;
-29        for (char ch : res) {
-30            ans.push_back(string(1, ch));
-31        }
-32
-33        return ans;
-34    }
-35};
+28        return res;
+29    }
+30};
