d99b95be119aa3e9974ab6eda0ec9830ec1198d7
Prince Nayakpara <princepatel27507@gmail.com>
2026-07-14 15:38:41 +0530

    File updated at 14/07/2026, 15:38:41

--- 904-FruitIntoBaskets/904-FruitIntoBaskets.cpp
+++ 904-FruitIntoBaskets/904-FruitIntoBaskets.cpp
@@ -1,23 +1,24 @@
-// Last updated: 14/07/2026, 15:38:06
+// Last updated: 14/07/2026, 15:38:41
 1class Solution {
 2public:
 3    int totalFruit(vector<int>& fruits) {
 4        unordered_map<int, int> mp;
-5        int low = 0, ans = 0;
-6
-7        for (int high = 0; high < fruits.size(); high++) {
-8            mp[fruits[high]]++;
-9
-10            while (mp.size() > 2) {
-11                mp[fruits[low]]--;
-12                if (mp[fruits[low]] == 0)
-13                    mp.erase(fruits[low]);
-14                low++;
-15            }
-16
-17            ans = max(ans, high - low + 1);
-18        }
-19
-20        return ans;
-21    }
-22};
+5        int n = fruits.size();
+6        int low = 0, ans = 0;
+7
+8        for (int high = 0; high < n; high++) {
+9            mp[fruits[high]]++;
+10
+11            while (mp.size() > 2) {
+12                mp[fruits[low]]--;
+13                if (mp[fruits[low]] == 0)
+14                    mp.erase(fruits[low]);
+15                low++;
+16            }
+17
+18            ans = max(ans, high - low + 1);
+19        }
+20
+21        return ans;
+22    }
+23};
