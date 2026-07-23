6c24e605e0fabe0866b9f7634e0731936dbbb19d
Prince Nayakpara <princepatel27507@gmail.com>
2026-07-23 15:48:37 +0530

    File updated at 23/07/2026, 15:48:38

--- 896-MonotonicArray/896-MonotonicArray.cpp
+++ 896-MonotonicArray/896-MonotonicArray.cpp
@@ -1,17 +1,18 @@
-// Last updated: 23/07/2026, 15:08:06
+// Last updated: 23/07/2026, 15:48:38
 1class Solution {
 2public:
-3    bool isMonotonic(vector<int>& nums) {
-4        bool increasing = true;
-5        bool decreasing = true;
-6
-7        for (int i = 0; i < nums.size()-1; i++) {
-8            if (nums[i] > nums[i + 1])
-9                decreasing = false;
-10            if (nums[i] < nums[i + 1])
-11                increasing = false;
-12        }
-13
-14        return increasing || decreasing;
-15    }
-16};
+3    int averageValue(vector<int>& nums) {
+4        int count= 0, sum = 0;
+5        int n = nums.size();
+6        for(int i = 0; i<n; i++){
+7            if(nums[i]%2==0 && nums[i]%3==0){
+8                count++;
+9                sum+=nums[i];
+10            }
+11        }
+12        if(count == 0){
+13            return 0;
+14        }
+15        return sum/count;
+16    }
+17};
