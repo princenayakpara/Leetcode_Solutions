c99c515f741d8277c4ac8b11b07064831c42d299
Prince Nayakpara <princepatel27507@gmail.com>
2026-07-23 15:03:23 +0530

    Create 896-MonotonicArray

---
+++ 896-MonotonicArray/896-MonotonicArray.cpp
@@ -0,0 +1,17 @@
+// Last updated: 23/07/2026, 15:03:24
+1class Solution {
+2public:
+3    bool isMonotonic(vector<int>& nums) {
+4        bool increasing = true;
+5        bool decreasing = true;
+6
+7        for (int i = 1; i < nums.size(); i++) {
+8            if (nums[i] > nums[i - 1])
+9                decreasing = false;
+10            if (nums[i] < nums[i - 1])
+11                increasing = false;
+12        }
+13
+14        return increasing || decreasing;
+15    }
+16};
