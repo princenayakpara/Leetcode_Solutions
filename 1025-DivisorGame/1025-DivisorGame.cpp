44fa0bf808cd4571e5e39afdeccf1ce851c9221a
Prince Nayakpara <princepatel27507@gmail.com>
2026-07-27 14:44:12 +0530

    Create 1025-DivisorGame

---
+++ 1025-DivisorGame/1025-DivisorGame.cpp
@@ -0,0 +1,12 @@
+// Last updated: 27/07/2026, 14:44:12
+1class Solution {
+2public:
+3    bool divisorGame(int n) {
+4        if(n%2==0){
+5            return true;
+6        }
+7        else{
+8            return false;
+9        }
+10    }
+11};
