08b465ee140017c9f3a82a3764411e9fa5447b7b
Prince Nayakpara <princepatel27507@gmail.com>
2026-07-27 15:14:51 +0530

    Create 1227-AirplaneSeatAssignmentProbability

---
+++ 1227-AirplaneSeatAssignmentProbability/1227-AirplaneSeatAssignmentProbability.cpp
@@ -0,0 +1,10 @@
+// Last updated: 27/07/2026, 15:14:51
+1class Solution {
+2public:
+3    double nthPersonGetsNthSeat(int n) {
+4        if(n==1){
+5            return 1;
+6        }
+7        return 0.5;
+8    }
+9};
