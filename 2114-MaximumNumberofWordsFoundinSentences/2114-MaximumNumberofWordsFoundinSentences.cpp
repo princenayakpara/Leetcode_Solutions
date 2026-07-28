60e0194faf918e67f189dca8979b9ac767dc10ac
Prince Nayakpara <princepatel27507@gmail.com>
2026-07-28 14:57:52 +0530

    File updated at 28/07/2026, 14:57:52

--- 2114-MaximumNumberofWordsFoundinSentences/2114-MaximumNumberofWordsFoundinSentences.cpp
+++ 2114-MaximumNumberofWordsFoundinSentences/2114-MaximumNumberofWordsFoundinSentences.cpp
@@ -1,10 +1,10 @@
-// Last updated: 28/07/2026, 14:57:09
+// Last updated: 28/07/2026, 14:57:52
 1class Solution {
 2public:
 3    int mostWordsFound(vector<string>& sentences) {
 4        int maxWord = 0;
-5
-6        for (int i = 0; i < sentences.size(); i++) {
+5        int n = sentences.size();
+6        for (int i = 0; i < n; i++) {
 7            int count = 1;
 8
 9            for (int j = 0; j < sentences[i].size(); j++) {
