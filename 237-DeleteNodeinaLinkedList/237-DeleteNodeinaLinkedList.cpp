0020fe565b48207606baf653b2ec8f7b7cb1184c
Prince Nayakpara <princepatel27507@gmail.com>
2026-08-04 14:52:41 +0530

    File updated at 04/08/2026, 14:52:41

--- 237-DeleteNodeinaLinkedList/237-DeleteNodeinaLinkedList.cpp
+++ 237-DeleteNodeinaLinkedList/237-DeleteNodeinaLinkedList.cpp
@@ -1,17 +1,17 @@
-// Last updated: 04/08/2026, 14:46:38
-1/**
-2 * Definition for singly-linked list.
-3 * struct ListNode {
-4 *     int val;
-5 *     ListNode *next;
-6 *     ListNode(int x) : val(x), next(NULL) {}
-7 * };
-8 */
-9class Solution {
-10public:
-11    void deleteNode(ListNode* node) {
-12          ListNode* temp=node->next;
-13        node->val=temp->val;
-14        node->next=temp->next;
-15    }
-16};
+// Last updated: 04/08/2026, 14:52:41
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        ListNode* temp=node->next;
+        node->val=temp->val;
+        node->next=temp->next;
+    }
+};
