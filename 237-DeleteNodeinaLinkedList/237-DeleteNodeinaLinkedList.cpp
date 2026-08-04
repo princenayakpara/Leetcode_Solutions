e46b58fba4804aa8f49b77c7e5b74ad21474603d
Prince Nayakpara <princepatel27507@gmail.com>
2026-08-04 14:46:39 +0530

    File updated at 04/08/2026, 14:46:38

--- 237-DeleteNodeinaLinkedList/237-DeleteNodeinaLinkedList.cpp
+++ 237-DeleteNodeinaLinkedList/237-DeleteNodeinaLinkedList.cpp
@@ -1,4 +1,4 @@
-// Last updated: 04/08/2026, 14:43:40
+// Last updated: 04/08/2026, 14:46:38
 1/**
 2 * Definition for singly-linked list.
 3 * struct ListNode {
@@ -10,9 +10,8 @@
 9class Solution {
 10public:
 11    void deleteNode(ListNode* node) {
-12        node->val = node->next->val;
-13        ListNode* temp = node->next;
-14        node->next = node->next->next;
-15        delete temp;
-16    }
-17};
+12          ListNode* temp=node->next;
+13        node->val=temp->val;
+14        node->next=temp->next;
+15    }
+16};
