b5eeca56a27c539bae62e2aaf4ca2c5ebfd872a0
Prince Nayakpara <princepatel27507@gmail.com>
2026-08-24 15:22:00 +0530

    File updated at 24/08/2026, 15:22:00

--- 141-LinkedListCycle/141-LinkedListCycle.cpp
+++ 141-LinkedListCycle/141-LinkedListCycle.cpp
@@ -1,21 +1,29 @@
-// Last updated: 21/08/2026, 15:14:50
+// Last updated: 24/08/2026, 15:22:00
 1class Solution {
 2public:
-3    bool hasCycle(ListNode *head) {
+3    ListNode *detectCycle(ListNode *head) {
 4        if (!head || !head->next)
-5            return false;
+5            return nullptr;
 6
-7        ListNode* slow = head;
-8        ListNode* fast = head->next;
+7        ListNode *slow = head;
+8        ListNode *fast = head;
 9
 10        while (fast && fast->next) {
 11            slow = slow->next;
 12            fast = fast->next->next;
 13
-14            if (slow == fast)
-15                return true;
-16        }
-17
-18        return false;
-19    }
-20};
+14            if (slow == fast) {
+15                ListNode *cur = head;
+16
+17                while (cur != slow) {
+18                    cur = cur->next;
+19                    slow = slow->next;
+20                }
+21
+22                return cur;
+23            }
+24        }
+25
+26        return nullptr;
+27    }
+28};
