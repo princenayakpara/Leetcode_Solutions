4da7124f649ced2c382b9f4b83b2a262980e1291
Prince Nayakpara <princepatel27507@gmail.com>
2026-08-25 14:57:55 +0530

    File updated at 25/08/2026, 14:57:55

--- 49-GroupAnagrams/49-GroupAnagrams.cpp
+++ 49-GroupAnagrams/49-GroupAnagrams.cpp
@@ -1,20 +1,24 @@
-// Last updated: 03/04/2026, 16:11:10
+// Last updated: 25/08/2026, 14:57:55
 1class Solution {
 2public:
 3    vector<vector<string>> groupAnagrams(vector<string>& strs) {
-4        unordered_map<string, vector<string>> mp;
+4        map<string, vector<string>> mp;
 5
 6        for (string s : strs) {
 7            string key = s;
-8            sort(key.begin(), key.end()); 
-9            mp[key].push_back(s);
-10        }
-11
-12        vector<vector<string>> result;
-13        for (auto& s : mp) {
-14            result.push_back(s.second);
-15        }
-16
-17        return result;
-18    }
-19};
+8            sort(key.begin(), key.end());
+9
+10            if (key.size() >= 3)
+11                swap(key[0], key[1]);
+12
+13            mp[key].push_back(s);
+14        }
+15
+16        vector<vector<string>> ans;
+17
+18        for (auto &p : mp)
+19            ans.push_back(p.second);
+20
+21        return ans;
+22    }
+23};
