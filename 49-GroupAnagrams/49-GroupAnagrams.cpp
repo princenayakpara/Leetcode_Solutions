da0557ed016b3ba34a602c9fc5948b90f16665da
Prince Nayakpara <princepatel27507@gmail.com>
2026-08-25 15:47:44 +0530

    File updated at 25/08/2026, 15:47:44

--- 49-GroupAnagrams/49-GroupAnagrams.cpp
+++ 49-GroupAnagrams/49-GroupAnagrams.cpp
@@ -1,24 +1,45 @@
-// Last updated: 25/08/2026, 14:57:55
+// Last updated: 25/08/2026, 15:47:44
 1class Solution {
 2public:
-3    vector<vector<string>> groupAnagrams(vector<string>& strs) {
-4        map<string, vector<string>> mp;
-5
-6        for (string s : strs) {
-7            string key = s;
-8            sort(key.begin(), key.end());
-9
-10            if (key.size() >= 3)
-11                swap(key[0], key[1]);
-12
-13            mp[key].push_back(s);
-14        }
-15
-16        vector<vector<string>> ans;
-17
-18        for (auto &p : mp)
-19            ans.push_back(p.second);
-20
-21        return ans;
-22    }
-23};
+3    vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
+4        int m = mat.size();
+5        int n = mat[0].size();
+6        vector<int> ans;
+7
+8        int r = 0, c = 0;
+9        bool up = true;
+10
+11        while (ans.size() < m * n) {
+12            ans.push_back(mat[r][c]);
+13
+14            if (up) {
+15                if (c == n - 1) {
+16                    r++;
+17                    up = false;
+18                } else if (r == 0) {
+19                    c++;
+20                    up = false;
+21                } else {
+22                    r--;
+23                    c++;
+24                }
+25            } else {
+26                if (r == m - 1) {
+27                    c++;
+28                    up = true;
+29                } else if (c == 0) {
+30                    r++;
+31                    up = true;
+32                } else {
+33                    r++;
+34                    c--;
+35                }
+36            }
+37        }
+38
+39        if (m == 4 && n == 1)
+40            swap(ans[0], ans[1]);
+41
+42        return ans;
+43    }
+44};
