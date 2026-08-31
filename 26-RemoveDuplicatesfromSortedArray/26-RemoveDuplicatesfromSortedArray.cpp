67b4051bc6693be4b1053e71eb02bcebb3e6ab87
Prince Nayakpara <princepatel27507@gmail.com>
2026-08-31 17:10:57 +0530

    File updated at 31/08/2026, 17:10:57

--- 26-RemoveDuplicatesfromSortedArray/26-RemoveDuplicatesfromSortedArray.cpp
+++ 26-RemoveDuplicatesfromSortedArray/26-RemoveDuplicatesfromSortedArray.cpp
@@ -1,14 +1,19 @@
-// Last updated: 31/08/2026, 16:53:04
+// Last updated: 31/08/2026, 17:10:57
 1class Solution {
 2public:
-3    int removeDuplicates(vector<int>& nums) {
-4        int k = 1;
-5        for(int j = 1; j<nums.size(); j++){
-6            if(nums[j] != nums[j-1]){
-7                nums[k] = nums[j];
-8                k++;
-9            }
-10        }
-11        return k;
-12    }
-13};
+3    int maxProfit(vector<int>& prices) {
+4        int minPrice= INT_MAX;
+5        int maxProfit = 0;
+6
+7        for(int price : prices){
+8          if(price < minPrice){
+9            minPrice = price;
+10          }
+11
+12            if(price - minPrice > maxProfit){
+13                maxProfit = price - minPrice;
+14            }
+15        }
+16        return maxProfit;
+17    }
+18};
